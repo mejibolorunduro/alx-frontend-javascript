7055259ca1c860c0ea86f8943bfb50e8d8c6b5fa
mejibolorunduro <davidbolorunduro2004@gmail.com>
2024-06-27 23:22:41 +0000

    ES6_classes -> completed

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -18,3 +18,23 @@ export default class SkyHighBuilding extends Building {
     return ;
   }
 }
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return ;
+  }
+}
